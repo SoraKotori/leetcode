187f6862cc0163a3a5d92a2725502143fd805f6f
SoraKotori <38377583+SoraKotori@users.noreply.github.com>
2025-01-31 13:45:41 +0000

    refactor Making A Large Island solution to enhance logic and handle edge cases

--- 827. Making A Large Island.cpp
+++ 827. Making A Large Island.cpp
@@ -42,9 +42,7 @@ public:
         int max_size = 0;
         for (int i = 0 ; i < m; i++)
             for (int j = 0; j < n; j++)
-                if (grid[i][j])
-                    max_size = max(max_size, island_size[i * n + j]);
-                else
+                if (grid[i][j] == 0)
                 {
                     int island = 1;
                     set<int> island_root;
@@ -67,7 +65,7 @@ public:
                     max_size = max(max_size, island);
                 }
 
-        return max_size;
+        return max_size ? max_size : m * n;
     }
 };
 
@@ -109,3 +107,31 @@ int main() {
 
     return 0;
 }
+
+// int get(int i, int j, vector<vector<int>>& g) {
+//   return (min(i, j) < 0 || i >= g.size() || j >= g[0].size()) ? 0 : g[i][j];
+// }
+// int paint(int i, int j, int clr, vector<vector<int>>& g) {
+//   if (get(i, j, g) != 1)
+//     return 0;
+//   g[i][j] = clr;
+//   return 1 + paint(i + 1, j, clr, g) + paint(i - 1, j, clr, g) +
+//              paint(i, j + 1, clr, g) + paint(i, j - 1, clr, g);
+// }
+// int largestIsland(vector<vector<int>>& g, int res = 0) {
+//   vector<int> sizes = {0, 0};  // sentinel values; colors start from 2.
+//   for (auto i = 0; i < g.size(); ++i)
+//     for (auto j = 0; j < g[i].size(); ++j)
+//       if (g[i][j] == 1)
+// 	    sizes.push_back(paint(i, j, sizes.size(), g));
+//   for (auto i = 0; i < g.size(); ++i)
+//     for (auto j = 0; j < g[i].size(); ++j)
+//       if (g[i][j] == 0) {
+//         unordered_set<int> s = { get(i + 1, j, g), get(i - 1, j, g),
+//                                  get(i, j + 1, g), get(i, j - 1, g) };
+//         res = max(res, accumulate(begin(s), end(s), 1, [&](int sum, int col) {
+//           return sum + sizes[col];
+//         }));
+//       }
+//   return res == 0 ? g.size() * g[0].size() : res;
+// }
