431226e8a8b5ea76f0ba1c61f5d0ddce188c308a
SoraKotori <38377583+SoraKotori@users.noreply.github.com>
2025-02-04 01:14:29 +0000

    add example test cases for Maximum Ascending Subarray Sum solution

--- 1800. Maximum Ascending Subarray Sum.cpp
+++ 1800. Maximum Ascending Subarray Sum.cpp
@@ -15,3 +15,18 @@ public:
         return max_value;
     }
 };
+
+int main() {
+    Solution solution;
+
+    vector<int> nums1 = {10, 20, 30, 5, 10, 50};
+    cout << "Example 1: " << solution.maxAscendingSum(nums1) << endl; // Output: 65
+
+    vector<int> nums2 = {10, 20, 30, 40, 50};
+    cout << "Example 2: " << solution.maxAscendingSum(nums2) << endl; // Output: 150
+
+    vector<int> nums3 = {12, 17, 15, 13, 10, 11, 12};
+    cout << "Example 3: " << solution.maxAscendingSum(nums3) << endl; // Output: 33
+
+    return 0;
+}
