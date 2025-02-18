221cea219d101961000f5302baa16c494024461b
SoraKotori <38377583+SoraKotori@users.noreply.github.com>
2025-02-18 13:25:30 +0000

    add solution for Construct the Lexicographically Largest Valid Sequence and update CMakeLists.txt

--- 1718. Construct the Lexicographically Largest Valid Sequence.cpp
+++ 1718. Construct the Lexicographically Largest Valid Sequence.cpp
@@ -27,17 +27,20 @@
 class Solution {
     vector<int> result;
 
-    bool gen(vector<int> current, vector<int>& other, int curr_i)
+    bool gen(vector<int>& current, vector<int>& other, int curr_i)
     {
         if (curr_i == size(current))
         {
             result = current;
             return true;
         }
-        else if (current[curr_i])
+
+        if (current[curr_i])
         {
-            if (gen(current, other, curr_i + 1))
-                return true;
+            return gen(current, other, curr_i + 1);
+
+            // if (gen(current, other, curr_i + 1))
+            //     return true;
         }
 
         for (int i = 0; i < size(other); i++)
@@ -45,12 +48,12 @@ class Solution {
             if (other[i])
                 continue;
             int num = size(other) - i;
+
             other[i] = true;
+            current[curr_i] = num;
             if (num == 1)
             {
-                auto new_current = current;
-                new_current[curr_i] = num;
-                if (gen(new_current, other, curr_i + 1))
+                if (gen(current, other, curr_i + 1))
                     return true;
             }
             else
@@ -58,13 +61,17 @@ class Solution {
                 int curr_j = curr_i + num;
                 if (curr_j < size(current) && current[curr_j] == 0)
                 {
-                    auto new_current = current;
-                    new_current[curr_i] = num;
-                    new_current[curr_j] = num;
-                    if (gen(new_current, other, curr_i + 1))
+                    // auto new_current = current;
+                    // new_current[curr_j] = num;
+                    current[curr_j] = num;
+
+                    if (gen(current, other, curr_i + 1))
                         return true;
+
+                    current[curr_j] = 0;
                 }
             }
+            current[curr_i] = 0;
             other[i] = false;
         }
 
@@ -74,9 +81,84 @@ public:
     vector<int> constructDistancedSequence(int n) {
 
         vector<int> other(n);
+        vector<int> current(n * 2 - 1);
 
-        gen(vector<int>(n * 2 - 1), other, 0);
+        gen(current, other, 0);
         return result;
+    }
+};
+
+class Solution_2 {
+public:
+    vector<int> constructDistancedSequence(int targetNumber) {
+        // Initialize the result sequence with size 2*n - 1 filled with 0s
+        vector<int> resultSequence(targetNumber * 2 - 1, 0);
+
+        // Keep track of which numbers are already placed in the sequence
+        vector<bool> isNumberUsed(targetNumber + 1, false);
+
+        // Start recursive backtracking to construct the sequence
+        findLexicographicallyLargestSequence(0, resultSequence, isNumberUsed,
+                                                targetNumber);
+
+        return resultSequence;
+    }
+
+private:
+    // Recursive function to generate the desired sequence
+    bool findLexicographicallyLargestSequence(int currentIndex,
+                                                vector<int>& resultSequence,
+                                                vector<bool>& isNumberUsed,
+                                                int targetNumber) {
+        // If we have filled all positions, return true indicating success
+        if (currentIndex == resultSequence.size()) {
+            return true;
+        }
+
+        // If the current position is already filled, move to the next index
+        if (resultSequence[currentIndex] != 0) {
+            return findLexicographicallyLargestSequence(
+                currentIndex + 1, resultSequence, isNumberUsed, targetNumber);
+        }
+
+        // Attempt to place numbers from targetNumber to 1 for a
+        // lexicographically largest result
+        for (int numberToPlace = targetNumber; numberToPlace >= 1;
+                numberToPlace--) {
+            if (isNumberUsed[numberToPlace]) continue;
+
+            isNumberUsed[numberToPlace] = true;
+            resultSequence[currentIndex] = numberToPlace;
+
+            // If placing number 1, move to the next index directly
+            if (numberToPlace == 1) {
+                if (findLexicographicallyLargestSequence(
+                        currentIndex + 1, resultSequence, isNumberUsed,
+                        targetNumber)) {
+                    return true;
+                }
+            }
+            // Place larger numbers at two positions if valid
+            else if (currentIndex + numberToPlace < resultSequence.size() &&
+                        resultSequence[currentIndex + numberToPlace] == 0) {
+                resultSequence[currentIndex + numberToPlace] = numberToPlace;
+
+                if (findLexicographicallyLargestSequence(
+                        currentIndex + 1, resultSequence, isNumberUsed,
+                        targetNumber)) {
+                    return true;
+                }
+
+                // Undo the placement for backtracking
+                resultSequence[currentIndex + numberToPlace] = 0;
+            }
+
+            // Undo current placement and mark the number as unused
+            resultSequence[currentIndex] = 0;
+            isNumberUsed[numberToPlace] = false;
+        }
+
+        return false;
     }
 };
 
@@ -87,5 +169,8 @@ int main()
     print_1D(sol.constructDistancedSequence(3));
     print_1D(sol.constructDistancedSequence(5));
     print_1D(sol.constructDistancedSequence(7));
+    print_1D(sol.constructDistancedSequence(9));
+    print_1D(sol.constructDistancedSequence(11));
     print_1D(sol.constructDistancedSequence(17));
+    print_1D(sol.constructDistancedSequence(19));
 }
